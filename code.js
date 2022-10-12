b73252617378e517152a5b5a9f671e18fe6177d6
DylanS6164 <112843279+DylanS6164@users.noreply.github.com>
2022-10-12 12:57:44 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,110 +1,34 @@
-//countdown function
-currTime = 50;
-var timeOut = 5000;
+//This function takes the countdown set up in the html page and changes the
+// the text 
 
 function runTimer(x) {
-    
+    currTime = 50;
+    var timeOut = 5000;
+    for(counter=0; counter < 11; counter++){
+        
+            setTimeout(function(){
+                if(currTime == 0){
+                x.innerHTML = "Blast Off"
+            } else{
+                x.innerHTML = currTime
+           }
+                currTime = currTime-5;
+            }, timeOut);
+            timeOut+=5000
+    }
+}    
+// This Fuction requests the users name and and will keep asking for the 
+// users name until the answer they provided is less than 15 characters
+function getUserInput() {
+
+    do{
+        userFirstName = prompt("Input User First Name:  ");
+        userLastName = prompt("Input User Last Name:  ");
+
+        user = username + " " + useranswer;
+        if (user.length > 15) {alert("Number of characters exceeds max limit")}
+    }while(user.length > 15)
+
+    alert("This is your Username:  " + user);
 }
-
-let x = document.getElementById("DisplayCountdown");
- setTimeout(function(){
-     x.innerHTML = currTime;
-     currTime = currTime-5;
- }, timeOut);
- timeOut = timeOut + 5000;
- 
   
- setTimeout(function(){
-     x.innerHTML = currTime
-     currTime = currTime-5;
- }, timeOut);
- timeOut = timeOut + 5000;
- 
-  
- setTimeout(function(){
-     x.innerHTML = currTime
-     currTime = currTime-5;
- }, timeOut);
- timeOut = timeOut + 5000;
- 
-  
- setTimeout(function(){
-     x.innerHTML = currTime
-     currTime = currTime-5;
- }, timeOut);
- timeOut = timeOut + 5000;
- 
-  
- setTimeout(function(){
-     x.innerHTML = currTime
-     currTime = currTime-5;
- }, timeOut);
- timeOut = timeOut + 5000;
- 
-  
- setTimeout(function(){
-     x.innerHTML = currTime
-     currTime = currTime-5;
- }, timeOut);
- timeOut = timeOut + 5000;
- 
-  
- setTimeout(function(){
-     x.innerHTML = currTime
-     currTime = currTime-5;
- }, timeOut);
- timeOut = timeOut + 5000;
- 
-  
- setTimeout(function(){
-     x.innerHTML = currTime
-     currTime = currTime-5;
- }, timeOut);
- timeOut = timeOut + 5000;
- 
- 
- setTimeout(function(){
-     x.innerHTML = currTime
-     currTime = currTime-5;
- }, timeOut);
- timeOut = timeOut + 5000;
- 
- 
- setTimeout(function(){
-     x.innerHTML = currTime
-     currTime = currTime-5;
- }, timeOut);
- timeOut = timeOut + 5000;
-
- setTimeout(function(){
-     x.innerHTML = "Blast Off"
- }, timeOut);
-
- //Craps Function
- function play() {
-    var die1 =Math.ceil(Math.random()*6);
-    
-    var die2 =Math.ceil(Math.random()*6);
-    
-    var sum = die1+die2
-    
-    
-    document.write("Die 1 = " + die1) 
-    document.write("<br/>") 
-    document.write("Die 2 = " + die2) 
-    document.write("<br/>") 
-    document.write("Sum = " + sum)
-    document.write("<br/>") 
-    if (sum == 7 || sum == 11) 
-    { 
-    
-    document.write("CRAPS - you lose") 
-    document.write("<br/>") 
-    }
-    else if (die1== die2 && die1%2 == 0)
-    {
-    document.write("DOUBLES - you win") 
-    document.write("<br/>") 
-    }
-    
-    }
